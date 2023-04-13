0be5a0648e6bc289fe855c65f9d65c746297e59a
Duncan Temple Lang <duncan@r-project.org>
2023-04-12 20:52:41 -0700

    extract code from day 3 and vital commands

---
+++ Lectures/Day3/essentialRcommands.R
@@ -0,0 +1,23 @@
+# Essential lines of code from Rsession_Day3
+# We'll turn them into functions.
+
+ll = readLines("NCBIQuery.txt")
+
+starts = which(substring(ll, 1, 7) == "Query #")
+ends = which(substring(ll, 1, nchar("Alignments:")) == "Alignments:")
+
+tt = ll [ (starts[1]+3):(ends[1]-1) ]
+tt = tt[ tt != "" ]
+
+colStarts = c(1, 67, 83, 99, 110, 117, 123, 129, 135, 142, 153)
+widths = diff(c(colStarts, nchar(tt[1])))
+
+dataLines = tt[-(1:2)]
+con = textConnection(dataLines)
+ans = read.fwf(con, widths)
+
+
+h = read.fwf(textConnection(tt[2]), widths)
+names(ans) = trimws(as.character(h))
+
+
