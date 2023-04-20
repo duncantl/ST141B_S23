b9b6276a5edb79450d6468a33a191932120dbd6d
Duncan Temple Lang <duncan@r-project.org>
2023-04-20 09:07:15 -0700

    comment to show problems

--- Data/NCBI/funs0.R
+++ Data/NCBI/funs0.R
@@ -1,28 +1,24 @@
 readTableData =
 function(txt, columnStarts)
 {
+    # assumes the columnStarts contains the length of the line to allow computing
+    # the width of the final column.
     widths = diff(columnStarts)
     con = textConnection(txt)
     read.fwf(con, widths = widths)
 }
 
 
-
-
-
-
-
-
-
-
 readTable =
     #
-    # Expects the header line
+    # Expects only the second header line and the data lines, i.e.,
+    # the first header line to be removed/absent by the caller
     #  readTable(tt[-1])
     #
 function(txt, header = txt[1], columnStarts = findColumnStarts(header))
 {
     ans = readTableData(txt[-1], columnStarts)
+    # XXX need to compute diff(columnStarts)
     vars = read.fwf(textConnection(header), columnStarts)
     names(ans) = vars
     ans
@@ -35,5 +31,6 @@ function(h)
     i = which(chars %in% LETTERS)
     w = which(chars %in% letters)
     st = chars[w - 1] == " "
+    # ) in wrong place
     sort(c(i, w[st]), nchar(h))
 }
